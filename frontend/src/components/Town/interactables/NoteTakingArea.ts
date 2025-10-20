9e8afc95aa130fbdbabd135a7c1de90fe5ac29e2
Ewan Lee <ewanlee519@gmail.com>
2025-10-20 05:20:12 -0400

    Updated NoteTakingArea.ts
    Changes:
    
     - Changed a lot of spacing issues for lint errors
     - Removed features copied over from ConversationArea not yet needed in NoteTakingArea
     - Implemented more features from ViewingArea instead
     - Gave background color, label text, and scene text to the interactable area
     - Displays label when player is over the box
     - Makes label disappear when interacting or leaving the box
     - Bare interactive area for NoteTaking implemented!

--- frontend/src/components/Town/interactables/NoteTakingArea.ts
+++ frontend/src/components/Town/interactables/NoteTakingArea.ts
@@ -1,27 +1,54 @@
-import NoteTakingAreaController from '../../controllers/interactables/NoteTakingAreaController';
-import { BoundingBox } from '../../../types/CoveyTownSocket';
 import Interactable, { KnownInteractableTypes } from '../Interactable';
 
 export default class NoteTakingArea extends Interactable {
-    private _noteTakingArea?: NoteTakingAreaController;
+  private _labelText?: Phaser.GameObjects.Text;
 
-    getType(): KnownInteractableTypes {
-        return 'noteTakingArea';
-    }
+  private _isInteracting = false;
 
-    removedFromScene(): void {
-        // No-op
-    }
+  getType(): KnownInteractableTypes {
+    return 'noteTakingArea';
+  }
 
-    addedToScene(): void {
-        super.addedToScene();
-    }
+  addedToScene() {
+    super.addedToScene();
+    this.setTintFill();
+    this.setAlpha(0.3);
+    this.setDepth(-1);
+    this.scene.add.text(
+      this.x - this.displayWidth / 2,
+      this.y + this.displayHeight / 2,
+      this.name,
+      { color: '#FFFFFF', backgroundColor: '#000000' },
+    );
+    this._labelText = this.scene.add.text(
+      this.x - this.displayWidth / 2,
+      this.y - this.displayHeight / 2,
+      `Press space to take notes`,
+      { color: '#FFFFFF', backgroundColor: '#000000' },
+    );
+    this._labelText.setVisible(false);
+  }
 
-    overlap(): void {
-        // No-op
-    }
+  interact(): void {
+    this._labelText?.setVisible(false);
+    this._isInteracting = true;
+  }
 
-    overlapExit(): void {
-        // No-op
+  overlap(): void {
+    if (!this._labelText) {
+      throw new Error('Should not be able to overlap with this interactable before added to scene');
     }
+    const location = this.townController.ourPlayer.location;
+    this._labelText.setX(location.x);
+    this._labelText.setY(location.y);
+    this._labelText.setVisible(true);
+  }
+
+  overlapExit(): void {
+    this._labelText?.setVisible(false);
+    if (this._isInteracting) {
+      this.townController.interactableEmitter.emit('endInteraction', this);
+      this._isInteracting = false;
+    }
+  }
 }
